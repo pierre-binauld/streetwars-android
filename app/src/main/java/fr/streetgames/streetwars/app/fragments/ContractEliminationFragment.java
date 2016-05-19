ab8a26cbeba5ede8d21fd85cbb47fa764f7d872b
Pierre Binauld <pierre.binauld@gmail.com>
2016-05-19 23:04:14 +0200

    Show elimination layout

--- app/src/main/java/fr/streetgames/streetwars/app/fragments/ContractEliminationFragment.java
+++ app/src/main/java/fr/streetgames/streetwars/app/fragments/ContractEliminationFragment.java
@@ -2,16 +2,24 @@ package fr.streetgames.streetwars.app.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import fr.streetgames.streetwars.R;
+import fr.streetgames.streetwars.widget.KillerAdapter;
 
 public class ContractEliminationFragment extends Fragment {
 
     public static final String TAG = "ContractEliminationFragment";
+
+    private RecyclerView mRecyclerView;
+
+    private RecyclerView.Adapter mAdapter;
 
     public ContractEliminationFragment() {
     }
@@ -28,5 +36,22 @@ public class ContractEliminationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contract_elimination, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.elimination_killer_list);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mAdapter = new KillerAdapter();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
